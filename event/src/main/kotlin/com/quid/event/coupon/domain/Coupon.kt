5120ebded33d671d217ecdec27234f57a1e64746
QuiD-0 <wodnd101@naver.com>
2023-10-12 17:53:15 +0900

    coupon 사용 테스트

--- event/src/main/kotlin/com/quid/event/coupon/domain/Coupon.kt
+++ event/src/main/kotlin/com/quid/event/coupon/domain/Coupon.kt
@@ -3,14 +3,28 @@ package com.quid.event.coupon.domain
 import java.time.LocalDate
 
 data class Coupon(
-    val id: String? = null,
+    val id: Long? = null,
+    val userId: Long,
     val couponName: String,
     val amount: CouponType,
     val regDate: LocalDate,
     val expireDate: LocalDate,
-    val isUsed: Boolean = false,
     val usedDate: LocalDate? = null
     ) {
+    val isUsed: Boolean
+        get() = usedDate != null
+    val isExpired: Boolean
+        get() = LocalDate.now().isAfter(expireDate)
+
+    init {
+        require(regDate.isBefore(expireDate)) { "regDate must be before expireDate" }
+    }
 
     fun discount(origin: Int): Int = amount.discount(origin)
+
+    fun useCoupon(): Coupon {
+        require(!isUsed) { "already used coupon" }
+        require(!isExpired) { "expired coupon" }
+        return this.copy(usedDate = LocalDate.now())
+    }
 }
--- event/src/test/kotlin/com/quid/event/coupon/domain/CouponTest.kt
+++ event/src/test/kotlin/com/quid/event/coupon/domain/CouponTest.kt
@@ -1,14 +1,19 @@
 package com.quid.event.coupon.domain
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.time.LocalDate
 
-class CouponTest{
+class CouponTest {
 
     @Test
-    fun typeTest(){
+    @DisplayName("쿠폰 할인 테스트")
+    fun typeTest() {
         val fixCoupon = Coupon(
+            userId = 1L,
             couponName = "FixedAmountCoupon",
             amount = FixedAmountCoupon(1000),
             regDate = LocalDate.now(),
@@ -16,6 +21,7 @@ class CouponTest{
         )
 
         val percentageCoupon = Coupon(
+            userId = 1L,
             couponName = "PercentageCoupon",
             amount = PercentageCoupon(20),
             regDate = LocalDate.now(),
@@ -24,5 +30,49 @@ class CouponTest{
 
         assertEquals(9000, fixCoupon.discount(10000))
         assertEquals(8000, percentageCoupon.discount(10000))
+    }
+
+    @Test
+    @DisplayName("쿠폰 만료일이 지났을 경우")
+    fun expiredTest() {
+        val coupon = Coupon(
+            userId = 1L,
+            couponName = "FixedAmountCoupon",
+            amount = FixedAmountCoupon(1000),
+            regDate = LocalDate.now().minusDays(7),
+            expireDate = LocalDate.now().minusDays(1)
+        )
+
+        assertTrue(coupon.isExpired)
+    }
+
+    @Test
+    @DisplayName("쿠폰 만료일은 등록일보다 빠를 수 없다.")
+    fun makeCouponFail() {
+        assertThrows<IllegalArgumentException> {
+            Coupon(
+                userId = 1L,
+                couponName = "FixedAmountCoupon",
+                amount = FixedAmountCoupon(1000),
+                regDate = LocalDate.now(),
+                expireDate = LocalDate.now().minusDays(1)
+            )
+        }
+    }
+
+    @Test
+    @DisplayName("쿠폰 사용 테스트")
+    fun useCouponTest() {
+        val coupon = Coupon(
+            userId = 1L,
+            couponName = "FixedAmountCoupon",
+            amount = FixedAmountCoupon(1000),
+            regDate = LocalDate.now(),
+            expireDate = LocalDate.now().plusDays(7)
+        )
+
+        val useCoupon = coupon.useCoupon()
+
+        assertTrue(useCoupon.isUsed)
     }
 }
