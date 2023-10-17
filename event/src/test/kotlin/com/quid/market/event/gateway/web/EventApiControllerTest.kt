a798e54ae37820649e2cd706c657fffc1b624bf3
QuiD-0 <wodnd101@naver.com>
2023-10-17 16:41:10 +0900

    논블로킹 테스트 적용

--- event/src/test/kotlin/com/quid/market/event/gateway/web/EventApiControllerTest.kt
+++ event/src/test/kotlin/com/quid/market/event/gateway/web/EventApiControllerTest.kt
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
+import java.lang.Thread.sleep
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
 
@@ -14,8 +16,7 @@ class EventApiControllerTest{
 
     @Test
     fun test(){
-        val url = "http://localhost:8080/api/events/coupon/issue"
-        val restTemplate = RestTemplate()
+        val client = client()
 
         val threadPool = Executors.newFixedThreadPool(32)
         val latch = CountDownLatch(100)
@@ -23,14 +24,26 @@ class EventApiControllerTest{
         for(i in 1..100){
             threadPool.submit {
                 try {
-                    val httpEntity = HttpEntity<IssueCouponRequest>(IssueCouponRequest(i.toLong(), 1))
-                    restTemplate.exchange(url,HttpMethod.POST,httpEntity, String::class.java)
-                } finally {
+                    client.post()
+                        .bodyValue(IssueCouponRequest(i.toLong(), 1))
+                        .retrieve()
+                        .bodyToMono(String::class.java)
+                        .log()
+                        .subscribe()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }finally {
                     latch.countDown()
                 }
             }
         }
         latch.await()
+
+        sleep(10000)
     }
+
+    private fun client() = WebClient.builder()
+        .baseUrl("http://localhost:8080/api/events/coupon/issue")
+        .build()
 }
 
