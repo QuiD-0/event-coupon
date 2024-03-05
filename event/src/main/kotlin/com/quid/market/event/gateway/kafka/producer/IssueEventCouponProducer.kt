5b91a45781db2766059d13eec087ee35d95de424
QuiD-0 <wodnd101@naver.com>
2024-03-05 11:02:39 +0900

    producer 생성

---
+++ event/src/main/kotlin/com/quid/market/event/gateway/kafka/producer/IssueEventCouponProducer.kt
@@ -0,0 +1,15 @@
+package com.quid.market.event.gateway.kafka.producer
+
+import com.quid.market.event.gateway.kafka.message.IssueCouponMessage
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class IssueEventCouponProducer(
+    private val kafkaTemplate: KafkaTemplate<String, IssueCouponMessage>
+) {
+
+    fun send(eventId: Long, userId: Long) {
+        kafkaTemplate.send("issue-event-coupon", IssueCouponMessage(eventId, userId))
+    }
+}
---
+++ event/src/main/kotlin/com/quid/market/global/KafkaConfig.kt
@@ -0,0 +1,35 @@
+package com.quid.market.global
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.*
+import org.springframework.kafka.support.serializer.JsonDeserializer
+import org.springframework.kafka.support.serializer.JsonSerializer
+
+@Configuration
+class KafkaConfig {
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, Any> {
+        return KafkaTemplate(producerConfigs())
+    }
+
+    @Bean
+    fun producerConfigs(): DefaultKafkaProducerFactory<String, Any> = mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java
+        ).let { DefaultKafkaProducerFactory(it) }
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, Any> = mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+            ConsumerConfig.GROUP_ID_CONFIG to "event-coupon",
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to JsonDeserializer::class.java
+        ).let { DefaultKafkaConsumerFactory(it) }
+}
